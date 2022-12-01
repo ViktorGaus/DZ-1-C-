4bb580c7e541cd22f9794f39fea1bfb7eaaeea1e
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-01 22:33:19 +0300

    Окончание задания

---
+++ assigment04/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число : ");
+int a = int.Parse(Console.ReadLine());
+int a0 = 2;
+if(a > 1)
+{
+    while(a0 <= a)
+    {
+    Console.Write($" {a0} " );
+    a0 = a0 + 2;
+    }
+}
