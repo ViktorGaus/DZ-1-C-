460402643c181aa05c6a462e07e821d183ea7d3c
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-01 21:30:46 +0300

    Задание 3

--- assigment02/Program.cs
+++ assigment02/Program.cs
@@ -6,6 +6,7 @@ int max = a;
 
 if(a > max) max = a;
 if(b > max) max = b;
+if(c > max) max = c;
 
 Console.Write("max = ");
 Console.WriteLine(max);
---
+++ assigment03/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число : ");
+int a = int.Parse(Console.ReadLine());
+
+if(a % 2 == 0)
+
+    Console.Write("Четное");
