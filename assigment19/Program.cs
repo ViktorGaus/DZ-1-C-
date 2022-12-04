f1cedbfbeb4481abe03d0ca37e65aac5a8c54d28
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-04 19:44:24 +0300

    Палиндром

---
+++ assigment19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите пятизначное число : ");
+int n = int.Parse(Console.ReadLine());
+
+if (n / 10000 == n % 10 || (n / 1000) % 10 == n % 100)  
+Console.Write("Является палиндромом");
+
+else if (n < 9999 || n > 99999)
+Console.WriteLine("Повторите попытку");
+
+else
+Console.Write("Не является палиндромом");
