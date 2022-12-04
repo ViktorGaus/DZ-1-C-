159a179252efb0358d3ee04d16071017ca91a2b2
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-04 14:19:49 +0300

    Выходной день

---
+++ assigment15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите цифру дня недели: ");
+int daynum = int.Parse(Console.ReadLine());
+
+if(daynum == 6 || daynum == 7)
+Console.WriteLine("Выходной день");
+
+else if(daynum < 1 || daynum > 7)
+Console.WriteLine("Повторите попытку");
+
+else
+Console.WriteLine("Рабочий день");
