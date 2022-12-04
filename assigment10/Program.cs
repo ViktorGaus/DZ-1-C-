15c8704d14b2b2768142688443c638db12132566
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-04 13:47:29 +0300

    Вывести 3-ю цифру

--- assigment10/Program.cs
+++ assigment10/Program.cs
@@ -3,4 +3,4 @@ Console.WriteLine($"Сгенерированное число {number}");
 
 int n = number / 10;
 
-Console.WriteLine(n / 10);
+Console.WriteLine(n % 10);
---
+++ assigment13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число : ");
+int number = int.Parse(Console.ReadLine());
+
+if(number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+else
+
+{
+    while(number > 999)
+    {
+        number /= 10;
+    }
+    Console.WriteLine(number % 10); 
+}
