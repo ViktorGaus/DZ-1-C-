f4f2362e7906e041f749b43b5dd8615900cf30ad
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-05 18:24:09 +0300

    Непонятно

--- assigment19/Program.cs
+++ assigment19/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите пятизначное число : ");
 int n = int.Parse(Console.ReadLine());
 
-if (n / 10000 == n % 10 || (n / 1000) % 10 == n % 100)  
+if (n / 10000 == n % 10 || (n / 1000) % 10 == (n % 100) / 10)  
 Console.Write("Является палиндромом");
 
 else if (n < 9999 || n > 99999)
