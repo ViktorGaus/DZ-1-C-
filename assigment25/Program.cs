1c5f7f8793c39be7970c62f101c78cb5010d9572
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-10 22:41:31 +0300

    assigment25

---
+++ assigment25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+int step = 1;
+
+for (int i = 0; i < b; i++)
+{
+step = step * a;
+} 
+
+
+Console.WriteLine(step);
