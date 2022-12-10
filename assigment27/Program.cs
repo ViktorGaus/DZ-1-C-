ab441558c882a6801ccab8ec1bf45b234c50f2b6
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-11 00:02:24 +0300

    assigment27

---
+++ assigment27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+string? r = Console.ReadLine();
+int sum = 0;
+
+for (int i = 0; i < r.Length; i++)
+{
+    //if (char.IsNumber(s[i]))
+    //{
+        sum += int.Parse(r[i].ToString());
+
+    //}
+
+}
+
+Console.Write(sum);
