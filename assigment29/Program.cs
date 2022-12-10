01e6ffde0be2b0d55b615d2b2d98ce50360fa06b
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-11 00:23:38 +0300

    assigment29

---
+++ assigment29/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите количество элементов массива: ");
+int a = int.Parse(Console.ReadLine());
+
+int[] massiv = new int[a];
+
+for (int i = 0; i < massiv.Length; i++)
+    {
+
+    Console.WriteLine($"Введите {i + 1} элемент массива");
+    massiv[i] = int.Parse(Console.ReadLine());
+
+    }
+
+Console.WriteLine($"|| {string.Join(" , " , massiv)} ||");
