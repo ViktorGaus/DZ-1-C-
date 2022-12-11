c68916de76362e6f503c28a3788c3324e5f9e6eb
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-12 01:00:24 +0300

    assigment38

---
+++ assigment38/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {   
+    array[i] =new Random().Next(10, 100);
+
+    if (i < array.Length)
+    array[i] = array[i] / 10;
+    }  
+}
+
+
+void ReleaseArray(double[] array)
+{
+    double max = 0, min = 1000;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max) max = array[i];
+        if(array[i] < min) min = array[i];
+    }
+    double sub = Math.Round((max - min), 2);
+    Console.WriteLine($"Разница между максимальным и минимальным элементом массива {sub}");
+}
+
+Console.Write("Введите число элементов ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+
+InputArray(array);
+Console.WriteLine($"[{string.Join(" , ", array)}]");
+ReleaseArray(array);
