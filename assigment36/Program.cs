6eb3c46b28b9cdc50b7c414b0927ae3fb1f2d80c
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-11 22:01:53 +0300

    assigment36

---
+++ assigment36/Program.cs
@@ -0,0 +1,24 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 10);
+}
+
+void ReleaseArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i % 2 != 0)
+        sum = sum + array[i];
+    }
+    Console.WriteLine($"Сумма элементов нечетных позиций равна {sum}");
+}
+
+Console.Write("Введите число элементов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+InputArray(array);
+Console.WriteLine($"[{string.Join(" , ", array)}]");
+ReleaseArray(array);
