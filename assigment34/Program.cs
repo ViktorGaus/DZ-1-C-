c87164559ad32ed73fe1e7863cb95f6bd511a57b
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-11 21:38:15 +0300

    assigment34

---
+++ assigment34/Program.cs
@@ -0,0 +1,20 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+Console.Write("Введите число элементов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+InputArray(array);
+
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] % 2 == 0)
+result += 1;
+} 
+Console.WriteLine($"[{string.Join(" , ", array)}]");
+Console.WriteLine(result);
