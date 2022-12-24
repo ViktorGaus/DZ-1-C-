1771711552371a7891431422eab83121c6212c8c
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-24 23:17:49 +0300

    assigment54

---
+++ assigment54/Program.cs
@@ -0,0 +1,48 @@
+﻿void InputArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write($"{array[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void ResultArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int z = 0; z < array.GetLength(1) - 1; z++)
+            {
+                if (array[i, z] < array[i, z + 1])
+                {
+                    int result = array[i, z + 1];
+                    array[i, z + 1] = array[i, z];
+                    array[i, z] = result;
+                }
+            }
+        }
+    }
+}
+
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] array = new int[size[0], size[1]];
+Console.WriteLine("Начальный массив: ");
+
+InputArray(array);
+PrintArray(array);
+Console.WriteLine("Конечный массив ");
+ResultArray(array);
+PrintArray(array);
