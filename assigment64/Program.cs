f47f9f90e35b48017fee63f49e90fb57600fa420
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-25 18:59:13 +0300

    assigment64

---
+++ assigment64/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите M: ");
+int m=int.Parse(ReadLine());
+Write("Введите N: ");
+int n=int.Parse(ReadLine());
+WriteLine(PrintNumbers(m, n));
+string PrintNumbers(int start, int end)
+{
+if (start == end)
+    return start.ToString();
+return (end + " " + PrintNumbers(start, end - 1));
+}
