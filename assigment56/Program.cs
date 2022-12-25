2ad519696a7a119a0a1df7ea94ac2df41e791609
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-25 11:04:53 +0300

    assigment56

---
+++ assigment56/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Common;
+
+int[,] matrix = new int[,]
+{
+    {3, 1, 5,},
+    {2, 9, 8,},
+    {1, 0, 2,},
+    {7, 2, 9,},
+};
+int row = 0;
+int minsum = 100;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+int sum = 0;
+for (int j = 0; j < matrix.GetLength(1); j++)
+sum += matrix[i,j];
+if(sum < minsum)
+    {
+    minsum = sum;
+    row = i+1;
+    }
+
+}
+Console.WriteLine("Строка с минимальной суммой номер ");
+Console.WriteLine(row);
