3a2c186d57c86c4253c2a18dbc3787b588abb87f
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-25 17:27:23 +0300

    assigment62

--- assigment60/Program.cs
+++ assigment60/Program.cs
@@ -1,13 +1,14 @@
 ﻿
 void InputMatrix(int[,,] matrix)
 {
+  int count = 10;
   for (int i = 0; i < matrix.GetLength(0); i++)
   {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
       for (int k = 0; k < matrix.GetLength(2); k++)
       {
-       matrix[i, j, k] = new Random().Next(10, 100);
+      matrix[i, j, k] += count++;
       }
     }
   }     
@@ -48,7 +49,16 @@ Console.Clear();
 Console.Write("Введите размеры матрицы: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int[,,] matrix = new int[size[0], size[1], size[2]];
-Console.WriteLine("Mатрица: ");
-InputMatrix(matrix);
-PrintMatrix(matrix);
-ResultMatrix(matrix);
+
+if (size[0] * size[1] * size[2] > 90)
+{
+  Console.WriteLine("Ошибка");
+}
+else
+{
+  Console.WriteLine("Mатрица: ");
+  InputMatrix(matrix);
+  PrintMatrix(matrix);
+  ResultMatrix(matrix);
+}
+
---
+++ assigment62/Program.cs
@@ -0,0 +1,33 @@
+﻿void PrintMatrix (int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      if (matrix[i,j] / 10 <= 0)
+      Console.Write($" {matrix[i,j]} \t");
+
+      else Console.Write($"{matrix[i,j]} \t");
+    }
+    Console.WriteLine();
+  }
+}
+
+int a = 4;
+int[,] matrix = new int[a, a];
+int count = 1;
+int i = 0;
+int j = 0;
+
+while (count <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = count;
+  count++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1) j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1) i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1) j--;
+  else i--;
+}
+
+PrintMatrix(matrix);
+
