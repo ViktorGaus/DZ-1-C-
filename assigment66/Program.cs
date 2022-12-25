3802fbc854504cd8b89b44876723af6ea15d1b32
ViktorGaus <118914679+ViktorGaus@users.noreply.github.com>
2022-12-25 19:28:54 +0300

    assigment66

---
+++ assigment66/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите первое число : ");
+int m=int.Parse(ReadLine());
+Write("Введите второе число: ");
+int n=int.Parse(ReadLine());
+
+WriteLine($"{Sum(m, n)}");
+
+int Sum(int m, int n)
+{
+if (n == m) return n;
+return m + Sum(m + 1, n);
+}
